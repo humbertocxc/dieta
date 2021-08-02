badcb85d4513035def1ea8e2bdf18ccbf1376c65
Humberto Gessinger <humberto.gnm98@gmail.com>
2021-08-02 19:48:36 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.scss'
+import Calculadora from './components/calculadora/Calculadora'
+
+export default function App() {
+  return (
+    <div className="app">
+      <Calculadora />
+    </div>
+  )
+}
---
+++ src/components/calculadora/Calculadora.jsx
@@ -0,0 +1,9 @@
+import './Calculadora'
+
+export default function Calculadora() {
+    return (
+        <div className="calculadora">
+            <h1>Estou na calculadora</h1>
+        </div>
+    )
+}

172dec4c5fa6d9ee40a191c2ceac25ae740eb3e5
Humberto Gessinger <humberto.gnm98@gmail.com>
2021-08-02 21:30:45 -0300

    primeiro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.scss'
 import Calculadora from './components/calculadora/Calculadora'
+import Sidebar from './components/sidebar/Sidebar'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
 
 export default function App() {
   return (
     <div className="app">
+      <Header />
       <Calculadora />
+      <Sidebar />
+      <Header />
     </div>
   )
 }
